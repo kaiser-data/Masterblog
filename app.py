451e6d15381d2ab8a9a6c4413db69eac2a1f0f7c
kaiser-data <61822885+kaiser-data@users.noreply.github.com>
2025-02-01 23:01:43 +0100

    added Readme and gitignore

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+[
+sample_structure    {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
+    {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
+    # More blog posts can go here...
+]
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
